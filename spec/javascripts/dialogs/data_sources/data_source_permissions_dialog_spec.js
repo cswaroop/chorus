0dc3172b08a1ac49f0c60ec5d11eb09e67f85ad2
Michel Milano <mmilano+git@gmail.com>
2015-01-16 17:33:35 -0800

    simplification of the spec

--- spec/javascripts/dialogs/data_sources/data_source_permissions_dialog_spec.js
+++ spec/javascripts/dialogs/data_sources/data_source_permissions_dialog_spec.js
@@ -125,7 +125,8 @@ describe("chorus.dialogs.DataSourcePermissions", function() {
             it("displays the shared account subheader", function() {
 //                     expect(this.dialog.$(".sub_header .details_text").text()).toMatchTranslation("data_sources.shared_account");
 //                  TO DO delete when final
-                 expect(this.dialog.$(".sub_header .shared_accounts").text()).toMatchTranslation("data_sources.shared_account");
+//                  expect(this.dialog.$(".sub_header .shared_accounts").text()).toMatchTranslation("data_sources.shared_account");
+                 expect(this.dialog.$(".shared_accounts").text()).toMatchTranslation("data_sources.shared_account");
             });
 
             it("displays the account owner information", function() {
